59665c41ff7953bffb056fd232b8904264b1c8da
ehynds@asu.edu <ehynds@asu.edu>
2022-06-18 23:59:39 -0400

    Remake migrations

--- server/src/services/db/migrations/20220608162909_init.ts
+++ server/src/services/db/migrations/20220608162909_init.ts
@@ -11,8 +11,92 @@ export async function up(knex: Knex): Promise<void> {
       .unique()
       .defaultTo(knex.raw("(gen_random_uuid())"));
   });
+
+  await knex.schema.createTable("list", (table) => {
+    table
+      .uuid("id")
+      .primary()
+      .unique()
+      .defaultTo(knex.raw("(gen_random_uuid())"));
+
+    table.string("title");
+    table.datetime("created_at").notNullable().defaultTo(knex.fn.now());
+    table.uuid("user_id").notNullable().references("id").inTable("user");
+
+    table.uuid("list_parent_id").references("id").inTable("list");
+    table
+      .enu("list_type", [
+        "NEXT",
+        "SOMEDAY",
+        "WAITING",
+        "AGENDA",
+        "PROJECT_SUPPORT",
+        "REFERENCE",
+      ])
+      .notNullable()
+      .defaultTo("NEXT");
+  });
+
+  await knex.schema.createTable("list_item", (table) => {
+    table
+      .uuid("id")
+      .primary()
+      .unique()
+      .defaultTo(knex.raw("(gen_random_uuid())"));
+
+    table.string("title").notNullable();
+    table.boolean("is_done").notNullable().defaultTo(false);
+    table.text("notes");
+    table.datetime("created_at").notNullable().defaultTo(knex.fn.now());
+
+    table.uuid("list_id").references("id").inTable("list").onDelete("SET NULL");
+    table.uuid("user_id").notNullable().references("id").inTable("user");
+  });
+
+  await knex.schema.createTable("refresh_token", (table) => {
+    table.string("token_hash").notNullable().primary().unique();
+    table.datetime("expires", { useTz: false }).notNullable();
+    table.uuid("user_id").notNullable().references("id").inTable("user");
+  });
+
+  await knex.schema.createTable("project", (table) => {
+    table
+      .uuid("id")
+      .primary()
+      .unique()
+      .defaultTo(knex.raw("(gen_random_uuid())"));
+
+    table.string("title").notNullable();
+    table.datetime("created_at").notNullable().defaultTo(knex.fn.now());
+
+    table.uuid("user_id").notNullable().references("id").inTable("user");
+  });
+
+  await knex.schema.alterTable("list_item", (table) => {
+    table
+      .uuid("project_id")
+      .references("id")
+      .inTable("project")
+      .onDelete("SET NULL");
+  });
+
+  await knex.schema.alterTable("list", (table) => {
+    table
+      .uuid("project_id")
+      .references("id")
+      .inTable("project")
+      .onDelete("CASCADE");
+  });
+
+  await knex.schema.raw(
+    "ALTER TABLE list ADD CHECK ((list_type <> 'PROJECT_SUPPORT' AND title IS NOT NULL) OR (project_id IS NOT NULL))"
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("project");
+  await knex.schema.dropTableIfExists("refresh_token");
+  await knex.schema.dropTableIfExists("list_item");
+  await knex.schema.dropTableIfExists("list");
   await knex.schema.dropTableIfExists("user");
 }
--- server/src/services/db/migrations/20220608231320_addListUid.ts
+++
@@ -1,9 +0,0 @@
-import { Knex } from "knex";
-
-export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable("list", (table) => {
-    table.uuid("user_id").notNullable().references("id").inTable("user");
-  });
-}
-
-export async function down(knex: Knex): Promise<void> {}
