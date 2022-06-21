b57d3a4e82c3f5f825f445fa7fc8a4b1400759c7
Ethan Hynds <ehynds2077@gmail.com>
2022-06-20 20:29:25 -0400

    Fix cors again - this one should do it

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 
 export const app = express();
 
-const origin = "https//neptunegtd.com";
+const origin = "https://neptunegtd.com";
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", origin);
@@ -18,7 +18,12 @@ const origin = "https//neptunegtd.com";
 //   next();
 // });
 
-app.use(cors());
+app.use(
+  cors<Request>({
+    credentials: true,
+    origin,
+  })
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
