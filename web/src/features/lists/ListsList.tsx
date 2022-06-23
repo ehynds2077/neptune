ca7a226065f5e47bc42059dd67b34972873b56d7
Ethan Hynds <ehynds2077@gmail.com>
2022-06-22 22:01:06 -0400

    Implement list expand/collapse sublists

--- web/src/features/lists/ListsList.tsx
+++ web/src/features/lists/ListsList.tsx
@@ -1,9 +1,9 @@
 import { List_ListType } from "./ListType";
 import { useState } from "react";
-import { useDisclosure } from "@chakra-ui/react";
+import { Box, filter, useDisclosure } from "@chakra-ui/react";
 import { AddListModal } from "../../components/AddListModal";
 import { HStack } from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
+import { AddIcon, ChevronDownIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/react";
 import { IoMdTrash } from "react-icons/io";
 import { Link as RouterLink } from "react-router-dom";
@@ -51,51 +51,79 @@ export const ListsList = ({
     editOnOpen();
   };
 
-  const SubLists = ({ parentId }: { parentId: string | null }) => {
+  const ListRow = ({ list }: { list: ListType }) => {
+    const [expand, setExpand] = useState(true);
+    const hasChildren = filteredLists.some((l) => l.list_parent_id === list.id);
+    const margin = list.depth * 12;
+
     return (
       <>
-        {filteredLists
-          .filter((list) => list.list_parent_id === parentId)
-          .map((list, idx) => {
-            const margin = list.depth * 8;
-            return (
-              <>
-                <HStack ml={margin} key={idx}>
-                  <Button
-                    as={RouterLink}
-                    to={`/list/${list.id}`}
-                    justifyContent="start"
-                    w="full"
-                    key={list.id}
-                  >
-                    {list.title}
-                  </Button>
-                  {list.depth !== 0 && (
-                    <>
-                      <IconButton
-                        onClick={() => {
-                          handleShowEdit(list);
-                        }}
-                        aria-label="Edit List"
-                        icon={<IoPencil />}
-                      />
-                      <IconButton
-                        onClick={() => {
-                          handleConfirmDelete(list);
-                        }}
-                        aria-label="Delete List"
-                        icon={<IoMdTrash />}
-                      />
-                    </>
-                  )}
-                </HStack>
-                <SubLists parentId={list.id} />
-              </>
-            );
-          })}
+        <HStack
+          px={1}
+          _hover={{
+            _dark: { bg: "whiteAlpha.300" },
+            _light: { bg: "gray.200" },
+          }}
+          rounded="lg"
+          _light={{ bg: "gray.100" }}
+          _dark={{ bg: "whiteAlpha.200" }}
+          ml={margin}
+        >
+          {hasChildren && (
+            <IconButton
+              variant="ghost"
+              _focus={{ boxShadow: "none" }}
+              icon={expand ? <ChevronDownIcon /> : <ChevronRightIcon />}
+              onClick={() => setExpand((expand) => !expand)}
+              aria-label="Expand sublists"
+            />
+          )}
+          <Box
+            as={RouterLink}
+            to={`/list/${list.id}`}
+            p={4}
+            px={hasChildren ? 0 : 4}
+            justifyContent="start"
+            w="full"
+            key={list.id}
+          >
+            {list.title}
+          </Box>
+          {list.depth !== 0 && (
+            <>
+              <IconButton
+                variant="ghost"
+                onClick={() => {
+                  handleShowEdit(list);
+                }}
+                aria-label="Edit List"
+                icon={<IoPencil />}
+              />
+              <IconButton
+                variant="ghost"
+                onClick={() => {
+                  handleConfirmDelete(list);
+                }}
+                aria-label="Delete List"
+                icon={<IoMdTrash />}
+              />
+            </>
+          )}
+        </HStack>
+        {expand && <SubLists parentId={list.id} />}
       </>
     );
   };
+
+  const SubLists = ({ parentId }: { parentId: string | null }) => (
+    <>
+      {filteredLists
+        .filter((list) => list.list_parent_id === parentId)
+        .map((list, idx) => {
+          return <ListRow list={list} key={idx} />;
+        })}
+    </>
+  );
 
   return (
     <List w="full" mb={8} maxW="xl" spacing={3}>
