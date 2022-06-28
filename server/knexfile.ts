09ae0bef0ac474ade184ce20d82997d1a972732a
Ethan Hynds <ehynds2077@gmail.com>
2022-06-28 14:45:53 -0400

    Change knexfile config

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,11 +1,10 @@
-import { configuration } from "./src/config";
 import * as path from "path";
 
 const BASE_PATH = path.join(__dirname, "src", "services", "db");
 
 export const development = {
   client: "pg",
-  connection: configuration.DATABASE_URL,
+  connection: process.env.DATABASE_URL,
 
   // connection: {
   //   host: configuration.DB_HOST,
